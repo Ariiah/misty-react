1a69262df0bc42d1f62a7879e3c0b7bb93cef105
Kelly L <lock2846@gmail.com>
2018-06-08 10:36:43 -0600

    volume re-added

--- src/components/textComponent.js
+++ src/components/textComponent.js
@@ -1,55 +1,59 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 const axios = require('axios')
 
-const API = 'http://localhost:3000/tts'
-
-const onClick = (e) => {
-  e.preventDefault()
-
-  let msg = document.getElementsByClassName('message')[0].value
-  console.log('msg', msg);
-
-  fetch(API, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ message: msg })
-  })
-  .then((response) => {
-    console.log('responded', response);
-  })
-  .catch((err) => {
-    console.log('Error', err);
-  })
-}
-
 class TextComponent extends Component {
-  constructor() {
-    super()
-      this.state = {
-        misty: []
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      misty: []
+    }
+
   }
+
+  onClick(e){
+    e.preventDefault()
+
+    let msg = document.getElementsByClassName('message')[0].value
+    let vol = document.getElementsByClassName('volume')[0].value
+    console.log('msg', msg);
+    console.log('vol', vol);
+
+    fetch('http://localhost:3000/tts', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({message: msg, volume: vol})
+    }).then((response) => {
+      console.log('responded', response)
+    }).catch((err) => {
+      console.log('Error', err)
+    })
+  }
+
+  // onChange(e) {
+  //   e.preventDefault()
+  //
+  //   this.vol = document.getElementsByClassName('volume')[0].value
+  //   // console.log('vol', vol)
+  //
+  //   return vol
+  // }
 
   render() {
 
-    return(
-      <div>
-        <form>
-<<<<<<< HEAD
-        What to say? <input type="text" className="message" placeholder="Hello World" />
-        <br></br>
-        <br></br>
-        <button type="submit" onClick={onClick}>Click Me!</button>
-=======
-          What to say? <input type="text" name="speech" className="message" placeholder="Hello World" />
-          <button type="submit" onClick={onClick}>Click Me!</button>
->>>>>>> ea3ab0ae1b25635886630f156682baebf9c26ef5
-        </form>
-      </div>
-    )
+    return (<div>
+      <form>
+        What to say?
+        <input type="text" className="message" placeholder="Hello World"/>
+        <button type="submit" onClick={this.onClick}>Click Me!</button>
+        <br />
+        <br />
+        Volume
+        <input type='range' min='1' max='10' defaultValue='5' step='1' className='volume'/>
+      </form>
+    </div>)
   }
 
 }
